b31185d04ffafb78b8a408886d18fe7571e47643
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-07-25 11:08:38 -0300

    Add files via upload

---
+++ admin-banco/js/firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js (versão compatível com Firebase 8 - namespace global)
+const firebaseConfig = {
+  apiKey: "AIzaSyDlbEZfA_uAR1aoPZIr8T9B6KNcrwfMxm0",
+  authDomain: "retorno-seguros.firebaseapp.com",
+  projectId: "retorno-seguros",
+  storageBucket: "retorno-seguros.appspot.com",
+  messagingSenderId: "495712392972",
+  appId: "1:495712392972:web:e1e78aedc48bdeea48db29",
+  measurementId: "G-C6E44WXLPW"
+};
