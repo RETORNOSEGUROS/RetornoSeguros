fafa1a136e2a64c6cb23e2b89dfbaf84c69a71c0
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-07-31 15:45:13 -0300

    Update firebase-config.js

--- admin-banco/js/firebase-config.js
+++ admin-banco/js/firebase-config.js
@@ -1,10 +1,31 @@
-// firebase-config.js (versão compatível com Firebase 8 - namespace global)
-const firebaseConfig = {
-  apiKey: "AIzaSyDlbEZfA_uAR1aoPZIr8T9B6KNcrwfMxm0",
-  authDomain: "retorno-seguros.firebaseapp.com",
-  projectId: "retorno-seguros",
-  storageBucket: "retorno-seguros.appspot.com",
-  messagingSenderId: "495712392972",
-  appId: "1:495712392972:web:e1e78aedc48bdeea48db29",
-  measurementId: "G-C6E44WXLPW"
-};
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Negócios Fechados</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Firebase via CDN -->
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+  <script>
+    const firebaseConfig = {
+      apiKey: "AIzaSyDlbEZfA_uAR1aoPZIr8T9B6KNcrwfMxm0",
+      authDomain: "retorno-seguros.firebaseapp.com",
+      projectId: "retorno-seguros",
+      storageBucket: "retorno-seguros.appspot.com",
+      messagingSenderId: "495712392972",
+      appId: "1:495712392972:web:e1e78aedc48bdeea48db29",
+      measurementId: "G-C6E44WXLPW"
+    };
+    firebase.initializeApp(firebaseConfig);
+  </script>
+</head>
+<body>
+  <h2>📑 Negócios Fechados da Agência</h2>
+  <div id="listaNegociosFechados"></div>
+
+  <script src="js/negocios-fechados.js"></script>
+</body>
+</html>
