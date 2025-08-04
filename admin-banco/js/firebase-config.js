6dbaae483060eec34855853f1a5462a0d45fa780
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-08-04 17:48:53 -0300

    Update firebase-config.js

--- admin-banco/js/firebase-config.js
+++ admin-banco/js/firebase-config.js
@@ -7,9 +7,10 @@ const firebaseConfig = {
   messagingSenderId: "495712392972",
   appId: "1:495712392972:web:e1e78aedc48bdeea48db29",
   measurementId: "G-C6E44WXLPW"
+};
 
-  // ✅ Adiciona somente se ainda não foi inicializado
+// ✅ Inicializar o Firebase apenas se ainda não foi inicializado
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
-};
+}
 
