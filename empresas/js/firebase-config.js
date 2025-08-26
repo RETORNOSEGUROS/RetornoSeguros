b47a2f8f43e985282f263cf15eb22a4c119f917a
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-08-26 13:44:20 -0300

    Update firebase-config.js

--- empresas/js/firebase-config.js
+++ empresas/js/firebase-config.js
@@ -1,4 +1,5 @@
-// js/firebase-config.js
+// /empresas/js/firebase-config.js
+
 const firebaseConfig = {
   apiKey: "AIzaSyDlbEZfA_uAR1aoPZIr8T9B6KNcrwfMxm0",
   authDomain: "retorno-seguros.firebaseapp.com",
@@ -9,5 +10,11 @@ const firebaseConfig = {
   measurementId: "G-C6E44WXLPW"
 };
 
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
 
+// Variáveis globais (usadas em login.js, admin.js, empresa.js)
+const auth = firebase.auth();
+const db   = firebase.firestore();
 
+console.log("✅ Firebase carregado:", firebaseConfig.projectId);
