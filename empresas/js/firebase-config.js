9c5a73a4a993bae5f80351a722299e149b30ca2d
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-08-26 11:16:39 -0300

    Create firebase-config.js

---
+++ empresas/js/firebase-config.js
@@ -0,0 +1,13 @@
+// js/firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDlbEZfA_uAR1aoPZIr8T9B6KNcrwfMxm0",
+  authDomain: "retorno-seguros.firebaseapp.com",
+  projectId: "retorno-seguros",
+  storageBucket: "retorno-seguros.appspot.com",
+  messagingSenderId: "495712392972",
+  appId: "1:495712392972:web:e1e78aedc48bdeea48db29",
+  measurementId: "G-C6E44WXLPW"
+};
+
+
+
