6092b65e4b45bf71fe2b05c308cd9689f53d4808
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-08-26 11:50:56 -0300

    Update collections.js

--- empresas/js/collections.js
+++ empresas/js/collections.js
@@ -1,9 +1,7 @@
-// /empresas/js/collections.js
-// Constantes de coleções do módulo "Ambiente Empresa" (isolado do restante do sistema)
 const COL = {
-  EMPRESAS: 'c.empresas',          // raiz das empresas do módulo
-  USU_EMPRESA: 'c.usuarios_empresa',// vínculos usuário↔empresa (por usuário)
-  APOLICES: 'c.apolices',          // subcoleção dentro de cada empresa
-  COLABS: 'c.colaboradores',       // subcoleção dentro de cada empresa
-  CONVITES: 'c.convites',          // opcional (convites)
+  EMPRESAS: 'c.empresas',
+  USU_EMPRESA: 'c.usuarios_empresa',
+  APOLICES: 'c.apolices',
+  COLABS: 'c.colaboradores',
+  CONVITES: 'c.convites',
 };
