9ad329d2e61ac318360148f07b8ea7dbafe2b321
RETORNOSEGUROS <retorno@retornoseguros.com.br>
2025-08-26 11:42:20 -0300

    Create collections.js

---
+++ empresas/js/collections.js
@@ -0,0 +1,9 @@
+// /empresas/js/collections.js
+// Constantes de coleções do módulo "Ambiente Empresa" (isolado do restante do sistema)
+const COL = {
+  EMPRESAS: 'c.empresas',          // raiz das empresas do módulo
+  USU_EMPRESA: 'c.usuarios_empresa',// vínculos usuário↔empresa (por usuário)
+  APOLICES: 'c.apolices',          // subcoleção dentro de cada empresa
+  COLABS: 'c.colaboradores',       // subcoleção dentro de cada empresa
+  CONVITES: 'c.convites',          // opcional (convites)
+};
